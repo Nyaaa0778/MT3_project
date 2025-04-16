513dbf00a9516068d9755d19f7b3786c95c2b507
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2025-04-16 16:42:41 +0900

    取り組み前

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "LC1B_26_ヤマダナオ_タイトル";
+const char kWindowTitle[] = "LE2B_27_ヤマダナオ_0_2_確認課題";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
