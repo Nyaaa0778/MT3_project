a6180d84c7226f3a26c172272cc540f8e6cc41da
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2025-04-25 11:36:31 +0900

    正射影行列、透視投影行列、ビューポート変換行列を実装

--- main.cpp
+++ main.cpp
@@ -1,52 +1,196 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 const char kWindowTitle[] = "LE2B_27_ヤマダ_ナオ_1_0_確認課題";
+
+struct Matrix4x4 {
+  float m[4][4];
+};
+
+/// <summary>
+/// 正射影行列(3次元版)
+/// </summary>
+/// <param name="left"></param>
+/// <param name="top"></param>
+/// <param name="right"></param>
+/// <param name="bottom"></param>
+/// <param name="nearClip"></param>
+/// <param name="farClip"></param>
+/// <returns></returns>
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right,
+                                 float bottom, float nearClip, float farClip) {
+  Matrix4x4 result;
+  result.m[0][0] = 2.0f / (right - left);
+  result.m[0][1] = 0.0f;
+  result.m[0][2] = 0.0f;
+  result.m[0][3] = 0.0f;
+
+  result.m[1][0] = 0.0f;
+  result.m[1][1] = 2.0f / (top - bottom);
+  result.m[1][2] = 0.0f;
+  result.m[1][3] = 0.0f;
+
+  result.m[2][0] = 0.0f;
+  result.m[2][1] = 0.0f;
+  result.m[2][2] = 1.0f / (farClip - nearClip);
+  result.m[2][3] = 0.0f;
+
+  result.m[3][0] = (left + right) / (left - right);
+  result.m[3][1] = (top + bottom) / (bottom - top);
+  result.m[3][2] = nearClip / (nearClip - farClip);
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+/// <summary>
+/// 透視投影行列
+/// </summary>
+/// <param name="fovY"></param>
+/// <param name="aspectRatio"></param>
+/// <param name="nearClip"></param>
+/// <param name="farClip"></param>
+/// <returns></returns>
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio,
+                                   float nearClip, float farClip) {
+  Matrix4x4 result;
+  result.m[0][0] = 1.0f / aspectRatio * 1.0f / tanf(fovY / 2.0f);
+  result.m[0][1] = 0.0f;
+  result.m[0][2] = 0.0f;
+  result.m[0][3] = 0.0f;
+
+  result.m[1][0] = 0.0f;
+  result.m[1][1] = 1.0f / tanf(fovY / 2.0f);
+  result.m[1][2] = 0.0f;
+  result.m[1][3] = 0.0f;
+
+  result.m[2][0] = 0.0f;
+  result.m[2][1] = 0.0f;
+  result.m[2][2] = farClip / (farClip - nearClip);
+  result.m[2][3] = 1.0f;
+
+  result.m[3][0] = 0.0f;
+  result.m[3][1] = 0.0f;
+  result.m[3][2] = -nearClip * farClip / (farClip - nearClip);
+  result.m[3][3] = 0.0f;
+
+  return result;
+}
+
+/// <summary>
+/// ビューポート変換行列
+/// </summary>
+/// <param name="left"></param>
+/// <param name="top"></param>
+/// <param name="width"></param>
+/// <param name="height"></param>
+/// <param name="minDepth"></param>
+/// <param name="maxDepth"></param>
+/// <returns></returns>
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height,
+                             float minDepth, float maxDepth) {
+  Matrix4x4 result;
+  result.m[0][0] = width / 2.0f;
+  result.m[0][1] = 0.0f;
+  result.m[0][2] = 0.0f;
+  result.m[0][3] = 0.0f;
+
+  result.m[1][0] = 0.0f;
+  result.m[1][1] = -height / 2.0f;
+  result.m[1][2] = 0.0f;
+  result.m[1][3] = 0.0f;
+
+  result.m[2][0] = 0.0f;
+  result.m[2][1] = 0.0f;
+  result.m[2][2] = maxDepth - minDepth;
+  result.m[2][3] = 0.0f;
+
+  result.m[3][0] = left + width / 2.0f;
+  result.m[3][1] = top + height / 2.0f;
+  result.m[3][2] = minDepth;
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+// 行列の表示
+static const int kRowHeight = 30;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 &matrix,
+                        const char *label) {
+  Novice::ScreenPrintf(x, y, "%s", label);
+
+  for (int row = 0; row < 4; ++row) {
+    for (int column = 0; column < 4; ++column) {
+      Novice::ScreenPrintf(x + column * kColumnWidth, 30 + y + row * kRowHeight,
+                           "%6.02f", matrix.m[row][column]);
+    }
+  }
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+  // ライブラリの初期化
+  Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+  // キー入力結果を受け取る箱
+  char keys[256] = {0};
+  char preKeys[256] = {0};
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+  Matrix4x4 orthographicMatrix =
+      MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+  Matrix4x4 perspectiveFovMatrix =
+      MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
 
-		///
-		/// ↓更新処理ここから
-		///
+  Matrix4x4 viewportMatrix =
+      MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
-		///
-		/// ↑更新処理ここまで
-		///
+  // ウィンドウの×ボタンが押されるまでループ
+  while (Novice::ProcessMessage() == 0) {
+    // フレームの開始
+    Novice::BeginFrame();
 
-		///
-		/// ↓描画処理ここから
-		///
+    // キー入力を受け取る
+    memcpy(preKeys, keys, 256);
+    Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↑描画処理ここまで
-		///
+    ///
+    /// ↓更新処理ここから
+    ///
 
-		// フレームの終了
-		Novice::EndFrame();
+    ///
+    /// ↑更新処理ここまで
+    ///
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+    ///
+    /// ↓描画処理ここから
+    ///
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+    MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+
+    MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix,
+                       "perspectiveFovMatrix");
+
+    MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
+
+    ///
+    /// ↑描画処理ここまで
+    ///
+
+    // フレームの終了
+    Novice::EndFrame();
+
+    // ESCキーが押されたらループを抜ける
+    if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+      break;
+    }
+  }
+
+  // ライブラリの終了
+  Novice::Finalize();
+  return 0;
 }
