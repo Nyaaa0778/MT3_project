b092b83c1faaf95613732264e6eeb27583262a80
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2025-04-25 12:33:27 +0900

    アフィン変換行列を追加

--- main.cpp
+++ main.cpp
@@ -1,52 +1,256 @@
+#include "Vector3.h"
 #include <Novice.h>
 
 const char kWindowTitle[] = "LE2B_27_ヤマダ_ナオ_1_1_確認課題";
 
+struct Matrix4x4 {
+  float m[4][4];
+};
+
+/// <summary>
+/// 行列の積
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Multiply(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+  Matrix4x4 result;
+
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      result.m[i][j] = 0;
+      for (int k = 0; k < 4; ++k) {
+        result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+      }
+    }
+  }
+
+  return result;
+}
+
+/// <summary>
+/// 拡縮行列
+/// </summary>
+/// <param name="scale"></param>
+/// <returns></returns>
+Matrix4x4 MakeScaleMatrix(const Vector3 &scale) {
+  Matrix4x4 result;
+
+  result.m[0][0] = scale.x;
+  result.m[0][1] = 0.0f;
+  result.m[0][2] = 0.0f;
+  result.m[0][3] = 0.0f;
+  result.m[1][0] = 0.0f;
+  result.m[1][1] = scale.y;
+  result.m[1][2] = 0.0f;
+  result.m[1][3] = 0.0f;
+  result.m[2][0] = 0.0f;
+  result.m[2][1] = 0.0f;
+  result.m[2][2] = scale.z;
+  result.m[2][3] = 0.0f;
+  result.m[3][0] = 0.0f;
+  result.m[3][1] = 0.0f;
+  result.m[3][2] = 0.0f;
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+/// <summary>
+/// X軸周りの回転行列
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix4x4 MakeRotateXMatrix(float radian) {
+  Matrix4x4 result;
+  result.m[0][0] = 1.0f;
+  result.m[0][1] = 0.0f;
+  result.m[0][2] = 0.0f;
+  result.m[0][3] = 0.0f;
+  result.m[1][0] = 0.0f;
+  result.m[1][1] = std::cos(radian);
+  result.m[1][2] = std::sin(radian);
+  result.m[1][3] = 0.0f;
+  result.m[2][0] = 0.0f;
+  result.m[2][1] = -std::sin(radian);
+  result.m[2][2] = std::cos(radian);
+  result.m[2][3] = 0.0f;
+  result.m[3][0] = 0.0f;
+  result.m[3][1] = 0.0f;
+  result.m[3][2] = 0.0f;
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+/// <summary>
+/// Y軸周りの回転行列
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix4x4 MakeRotateYMatrix(float radian) {
+  Matrix4x4 result;
+  result.m[0][0] = std::cos(radian);
+  result.m[0][1] = 0.0f;
+  result.m[0][2] = -std::sin(radian);
+  result.m[0][3] = 0.0f;
+  result.m[1][0] = 0.0f;
+  result.m[1][1] = 1.0f;
+  result.m[1][2] = 0.0f;
+  result.m[1][3] = 0.0f;
+  result.m[2][0] = std::sin(radian);
+  result.m[2][1] = 0.0f;
+  result.m[2][2] = std::cos(radian);
+  result.m[2][3] = 0.0f;
+  result.m[3][0] = 0.0f;
+  result.m[3][1] = 0.0f;
+  result.m[3][2] = 0.0f;
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+/// <summary>
+/// Z軸周りの回転行列
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix4x4 MakeRotateZMatrix(float radian) {
+  Matrix4x4 result;
+  result.m[0][0] = std::cos(radian);
+  result.m[0][1] = std::sin(radian);
+  result.m[0][2] = 0.0f;
+  result.m[0][3] = 0.0f;
+  result.m[1][0] = -std::sin(radian);
+  result.m[1][1] = std::cos(radian);
+  result.m[1][2] = 0.0f;
+  result.m[1][3] = 0.0f;
+  result.m[2][0] = 0.0f;
+  result.m[2][1] = 0.0f;
+  result.m[2][2] = 1.0f;
+  result.m[2][3] = 0.0f;
+  result.m[3][0] = 0.0f;
+  result.m[3][1] = 0.0f;
+  result.m[3][2] = 0.0f;
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+/// <summary>
+/// 平行移動行列
+/// </summary>
+/// <param name="translate"></param>
+/// <returns></returns>
+Matrix4x4 MakeTranslateMatrix(const Vector3 &translate) {
+  Matrix4x4 result;
+  result.m[0][0] = 1.0f;
+  result.m[0][1] = 0.0f;
+  result.m[0][2] = 0.0f;
+  result.m[0][3] = 0.0f;
+  result.m[1][0] = 0.0f;
+  result.m[1][1] = 1.0f;
+  result.m[1][2] = 0.0f;
+  result.m[1][3] = 0.0f;
+  result.m[2][0] = 0.0f;
+  result.m[2][1] = 0.0f;
+  result.m[2][2] = 1.0f;
+  result.m[2][3] = 0.0f;
+  result.m[3][0] = translate.x;
+  result.m[3][1] = translate.y;
+  result.m[3][2] = translate.z;
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+/// <summary>
+/// アフィン変換行列
+/// </summary>
+/// <param name="scale"></param>
+/// <param name="rotate"></param>
+/// <param name="translate"></param>
+/// <returns></returns>
+Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3 &rotate,
+                           const Vector3 &translate) {
+
+  Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+
+  Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+  Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+  Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+
+  Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+
+  Matrix4x4 worldMatrix = Multiply(
+      Multiply(Multiply(Multiply(scaleMatrix, rotateXMatrix), rotateYMatrix),
+               rotateZMatrix),
+      translateMatrix);
+
+  return worldMatrix;
+}
+
+/// <summary>
+/// クロス積
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 Cross(const Vector3 &v1, const Vector3 &v2) {
+  Vector3 result{v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z,
+                 v1.x * v2.y - v1.y * v2.x};
+
+  return result;
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+  // ライブラリの初期化
+  Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+  // キー入力結果を受け取る箱
+  char keys[256] = {0};
+  char preKeys[256] = {0};
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+  Vector3 rotate{};
+  Vector3 translate{};
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+  // ウィンドウの×ボタンが押されるまでループ
+  while (Novice::ProcessMessage() == 0) {
+    // フレームの開始
+    Novice::BeginFrame();
 
-		///
-		/// ↓更新処理ここから
-		///
+    // キー入力を受け取る
+    memcpy(preKeys, keys, 256);
+    Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↑更新処理ここまで
-		///
+    ///
+    /// ↓更新処理ここから
+    ///
 
-		///
-		/// ↓描画処理ここから
-		///
+    ///
+    /// ↑更新処理ここまで
+    ///
 
-		///
-		/// ↑描画処理ここまで
-		///
+    ///
+    /// ↓描画処理ここから
+    ///
 
-		// フレームの終了
-		Novice::EndFrame();
+    ///
+    /// ↑描画処理ここまで
+    ///
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+    // フレームの終了
+    Novice::EndFrame();
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+    // ESCキーが押されたらループを抜ける
+    if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+      break;
+    }
+  }
+
+  // ライブラリの終了
+  Novice::Finalize();
+  return 0;
 }
